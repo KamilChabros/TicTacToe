45250b608ea9ee4f255184bc802ab640991f4939
kamilchabros <kamilchabros@wp.pl.com>
2020-03-08 13:49:33 +0100

    Dodanie pętli for z ustawianiem pola X pod odpowiednim polem

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,42 +1,42 @@
 import java.util.Arrays;
 
 public class Board {
+    int boardDimension = 3;
+    char [][] board = new char[boardDimension][boardDimension];
 
-   void setField(int userChoice, char[][]board, char value){
+    void setField(int userChoice, char value){
         if(userChoice == 1){
             board[0][0] = value;
         }
-       if(userChoice == 2){
-           board[0][1] = value;
-       }
-       if(userChoice == 3){
-           board[0][2] = value;
-       }
-       if(userChoice == 4){
-           board[1][0] = value;
-       }
-       if(userChoice == 5){
-           board[1][1] = value;
-       }
-       if(userChoice == 6){
-           board[1][2] = value;
-       }
-       if(userChoice == 7){
-           board[2][0] = value;
-       }
-       if(userChoice == 8){
-           board[2][1] = value;
-       }
-       if(userChoice == 9){
-           board[2][2] = value;
-       }
+        if(userChoice == 2){
+            board[0][1] = value;
+        }
+        if(userChoice == 3){
+            board[0][2] = value;
+        }
+        if(userChoice == 4){
+            board[1][0] = value;
+        }
+        if(userChoice == 5){
+            board[1][1] = value;
+        }
+        if(userChoice == 6){
+            board[1][2] = value;
+        }
+        if(userChoice == 7){
+            board[2][0] = value;
+        }
+        if(userChoice == 8){
+            board[2][1] = value;
+        }
+        if(userChoice == 9){
+            board[2][2] = value;
+        }
     }
 
-    public static void main(String[] args) {
 
-       Board bd = new Board();
-        int boardDimension = 3;
-        char [][] board = new char[boardDimension][boardDimension];
+    {
+
 
         board[0][0] = '1';
         board[0][1] = '2';
@@ -47,7 +47,9 @@ public class Board {
         board[2][0] = '7';
         board[2][1] = '8';
         board[2][2] = '9';
+    }
 
+    void printBoard(){
         for (int i = 0; i < board.length; i++) {
             System.out.println("=======");
             for (int j = 0; j < board[i].length; j++) {
@@ -56,6 +58,5 @@ public class Board {
             }
             System.out.println("");
         }
-        System.out.println(bd.setField(5, board, 'o'));
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Game {
+
+    public static void main(String[] args) {
+
+        Board board = new Board();
+        Scanner sc = new Scanner(System.in);
+        char current = 'X';
+        char current2 = 'O';
+
+        System.out.println("Podaj proszę numer pola...");
+        for (int i = 0; i <9; i++){
+            int userChoice = sc.nextInt();
+            if (current == 'X'){
+                board.setField(userChoice, 'X');
+            } else if (current2 == 'O'){
+                board.setField(userChoice, 'O');
+            }
+            board.printBoard();
+        }
+    }
 }
