38af2dedb44d09976035da814a1fd3ac597e92bf
kamilchabros <kamilchabros@wp.pl.com>
2020-03-08 11:45:07 +0100

    Uzupełniona tablica board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class Board {
+    public static void main(String[] args) {
+
+        int boardDimension = 3;
+        char board[][] = new char[boardDimension][boardDimension];
+
+        board[0][0] = '1';
+        board[0][1] = '2';
+        board[0][2] = '3';
+        board[1][0] = '4';
+        board[1][1] = '5';
+        board[1][2] = '6';
+        board[2][0] = '7';
+        board[2][1] = '8';
+        board[2][2] = '9';
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.println("=======");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+                System.out.print("|");
+            }
+            System.out.println("");
+        }
+    }
 }
