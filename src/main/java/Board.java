8fd92c927c682a5093038e7ba5c5adfa711bc6e2
kamilchabros <kamilchabros@wp.pl.com>
2020-03-08 12:57:10 +0100

    Dodano klasę Game

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,10 +1,42 @@
 import java.util.Arrays;
 
 public class Board {
+
+   void setField(int userChoice, char[][]board, char value){
+        if(userChoice == 1){
+            board[0][0] = value;
+        }
+       if(userChoice == 2){
+           board[0][1] = value;
+       }
+       if(userChoice == 3){
+           board[0][2] = value;
+       }
+       if(userChoice == 4){
+           board[1][0] = value;
+       }
+       if(userChoice == 5){
+           board[1][1] = value;
+       }
+       if(userChoice == 6){
+           board[1][2] = value;
+       }
+       if(userChoice == 7){
+           board[2][0] = value;
+       }
+       if(userChoice == 8){
+           board[2][1] = value;
+       }
+       if(userChoice == 9){
+           board[2][2] = value;
+       }
+    }
+
     public static void main(String[] args) {
 
+       Board bd = new Board();
         int boardDimension = 3;
-        char board[][] = new char[boardDimension][boardDimension];
+        char [][] board = new char[boardDimension][boardDimension];
 
         board[0][0] = '1';
         board[0][1] = '2';
@@ -24,5 +56,6 @@ public class Board {
             }
             System.out.println("");
         }
+        System.out.println(bd.setField(5, board, 'o'));
     }
 }
