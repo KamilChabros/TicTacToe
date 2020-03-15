1159915d24f50c6d6aa4de3e7cf1bfccb74bf1a0
kamilchabros <kamilchabros@wp.pl.com>
2020-03-15 16:11:10 +0100

    Working code with checikng winner for 'X' player  -every combination.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -33,13 +34,35 @@ public class Game {
 
             if (activePlayer == 'X') {
                 board.setField(userChoice, 'X');
+                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == activePlayer * 3 ||
+                        board.board[2][0] + board.board[1][1] + board.board[0][2] == activePlayer * 3 ||
+                        // above is diagonal
+                        board.board[0][0] + board.board[0][1] + board.board[0][2] == activePlayer * 3 ||
+                        board.board[1][0] + board.board[1][1] + board.board[1][2] == activePlayer * 3 ||
+                        board.board[2][0] + board.board[2][1] + board.board[2][2] == activePlayer * 3 ||
+                        // above is row
+                        board.board[0][0] + board.board[1][0] + board.board[2][0] == activePlayer * 3 ||
+                        board.board[0][1] + board.board[1][1] + board.board[2][1] == activePlayer * 3 ||
+                        board.board[0][2] + board.board[1][2] + board.board[2][2] == activePlayer * 3)
+                // above is column
+                {
+                    System.out.println("You win!");
+                    System.out.println("Player 'X' win!");
+                    break;
+                }
             }
             if (activePlayer == 'O') {
                 board.setField(userChoice, 'O');
+                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == activePlayer * 3) {
+                    System.out.println("You win!");
+                    System.out.println("Player 'O' win!");
+                    break;
+                }
             }
 
             board.printBoard();
             counter++; // zmiana gracza
+
             if (counter % 2 == 0) {
                 activePlayer = 'O';
                 System.out.println("Teraz gracz O");
