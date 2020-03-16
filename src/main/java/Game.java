70e93097068c3c417c4893ece7bd5e7e0137fc5e
kamilchabros <kamilchabros@wp.pl.com>
2020-03-16 20:07:20 +0100

    Set nicknames, characters to play, checking all winning combinations for two players + some corrections in code.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,16 +10,33 @@ public class Game {
         Board board = new Board();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Witam w grze w kółko i krzyżyk.");
-        System.out.println("Wybierz numer odpowiadający polu, które chcesz wybrać.");
-        board.printBoard();
+        System.out.println("Welcome to Tic Tac Toe game.");
+        System.out.println("To place your sign, choose appropriate number from keyboard.");
         System.out.println();
-        System.out.println("Pierwszy gracz to X");
+        // Welcome, printing board and choosing information
 
-        char activePlayer = 'X';
-        int counter = 1;
+        System.out.println("Choose sign for First Player...(X, O, etc.)");
+        char firstSign = sc.next().charAt(0);
+        System.out.println("Provide nick for First Player: ");
+        String firstPLayerNickName = sc.next();
+        // First Player
 
-        System.out.println("Podaj proszę numer pola...");
+        System.out.println("Choose sign for Second Player...(X, O, etc.)");
+        char secondSign = sc.next().charAt(0);
+        System.out.println("Provide nick for Second Player: ");
+        String secondPLayerNickName = sc.next();
+        // Second Player
+        // to-do Checking if first player has the same sign
+
+        System.out.println("First Player is: " + firstPLayerNickName + " and has " + firstSign + " sign.");
+        System.out.println("Second Player is: " + secondPLayerNickName + " and has " + secondSign + " sign.");
+        // Choosing player, signs and entering nicknames
+
+        System.out.println("Please enter your number from keyboard...");
+        board.printBoard();
+
+        int counter = 0;
+
 
         Set<Integer> choices = new HashSet<>();
         while (choices.size() != 9) {
@@ -32,44 +49,48 @@ public class Game {
                 choices.add(userChoice);
             }
 
-            if (activePlayer == 'X') {
-                board.setField(userChoice, 'X');
-                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == activePlayer * 3 ||
-                        board.board[2][0] + board.board[1][1] + board.board[0][2] == activePlayer * 3 ||
-                        // above is diagonal
-                        board.board[0][0] + board.board[0][1] + board.board[0][2] == activePlayer * 3 ||
-                        board.board[1][0] + board.board[1][1] + board.board[1][2] == activePlayer * 3 ||
-                        board.board[2][0] + board.board[2][1] + board.board[2][2] == activePlayer * 3 ||
-                        // above is row
-                        board.board[0][0] + board.board[1][0] + board.board[2][0] == activePlayer * 3 ||
-                        board.board[0][1] + board.board[1][1] + board.board[2][1] == activePlayer * 3 ||
-                        board.board[0][2] + board.board[1][2] + board.board[2][2] == activePlayer * 3)
-                // above is column
-                {
-                    System.out.println("You win!");
-                    System.out.println("Player 'X' win!");
-                    break;
-                }
-            }
-            if (activePlayer == 'O') {
-                board.setField(userChoice, 'O');
-                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == activePlayer * 3) {
-                    System.out.println("You win!");
-                    System.out.println("Player 'O' win!");
-                    break;
-                }
-            }
+            if (counter % 2 == 0) {
+                board.setField(userChoice, firstSign);
 
+                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == firstSign * 3 ||
+                        board.board[2][0] + board.board[1][1] + board.board[0][2] == firstSign * 3 ||
+                        // above is checking diagonal
+                        board.board[0][0] + board.board[0][1] + board.board[0][2] == firstSign * 3 ||
+                        board.board[1][0] + board.board[1][1] + board.board[1][2] == firstSign * 3 ||
+                        board.board[2][0] + board.board[2][1] + board.board[2][2] == firstSign * 3 ||
+                        // above is checking row
+                        board.board[0][0] + board.board[1][0] + board.board[2][0] == firstSign * 3 ||
+                        board.board[0][1] + board.board[1][1] + board.board[2][1] == firstSign * 3 ||
+                        board.board[0][2] + board.board[1][2] + board.board[2][2] == firstSign * 3)
+                // above is checking column
+                {
+                    System.out.println("Player " + firstPLayerNickName + " win!");
+                    break;
+                }
+                System.out.println("Teraz gracz " + secondPLayerNickName);
+            } else if (counter % 2 != 0) {
+                board.setField(userChoice, secondSign);
+
+                if (board.board[0][0] + board.board[1][1] + board.board[2][2] == secondSign * 3 ||
+                        board.board[2][0] + board.board[1][1] + board.board[0][2] == secondSign * 3 ||
+                        // above is checking diagonal
+                        board.board[0][0] + board.board[0][1] + board.board[0][2] == secondSign * 3 ||
+                        board.board[1][0] + board.board[1][1] + board.board[1][2] == secondSign * 3 ||
+                        board.board[2][0] + board.board[2][1] + board.board[2][2] == secondSign * 3 ||
+                        // above is checking row
+                        board.board[0][0] + board.board[1][0] + board.board[2][0] == secondSign * 3 ||
+                        board.board[0][1] + board.board[1][1] + board.board[2][1] == secondSign * 3 ||
+                        board.board[0][2] + board.board[1][2] + board.board[2][2] == secondSign * 3)
+                // above is checking column
+                {
+                    System.out.println("Player " + secondPLayerNickName + " win!");
+                    break;
+                }
+                System.out.println("Teraz gracz " + firstPLayerNickName);
+            }
             board.printBoard();
             counter++; // zmiana gracza
 
-            if (counter % 2 == 0) {
-                activePlayer = 'O';
-                System.out.println("Teraz gracz O");
-            } else {
-                activePlayer = 'X';
-                System.out.println("Teraz gracz X");
-            }
         }
     }
 }
