592d9f024ec1a5b368e7ce4862889adb43619785
kamilchabros <kamilchabros@wp.pl.com>
2020-03-21 15:38:27 +0100

    Refactoring

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,19 +12,20 @@ public class Game {
 
         Player firstPlayer;
         Player secondPlayer;
+        Player currentPlayer;
 
         System.out.println("Choose mode: \n" +
                 "1 is: Player VS Player \n" +
-                "2 is: Player VS Cpu \n " +
+                "2 is: Player VS Cpu \n" +
                 "3 is: Cpu VS Cpu");
         int modeChoose = sc.nextInt();
         if (modeChoose == 1) {
             firstPlayer = new HumanPlayer(" ", ' ');
             secondPlayer = new HumanPlayer(" ", ' ');
-        }else if (modeChoose == 2) {
+        } else if (modeChoose == 2) {
             firstPlayer = new HumanPlayer(" ", ' ');
             secondPlayer = new CpuPlayer(" ", ' ');
-        } else (modeChoose == 3) {
+        } else if(modeChoose == 3) {
             firstPlayer = new CpuPlayer(" ", ' ');
             secondPlayer = new CpuPlayer(" ", ' ');
         }
@@ -39,40 +40,45 @@ public class Game {
         board.printBoard();
 
         int counter = 0;
+        if (counter % 2 == 0) {
+            currentPlayer = firstPlayer;
+        } else if (counter % 2 != 0) {
+            currentPlayer = secondPlayer;
 
-        Set<Integer> choices = new HashSet<>();
-        while (choices.size() != 9) {
-            int userChoice = sc.nextInt();
+            Set<Integer> choices = new HashSet<>();
+            while (choices.size() != 9) {
+                int userChoice = sc.nextInt();
 
-            if (choices.contains(userChoice)) {
-                System.out.println("This field ia unavailable, please choose another.");
-                continue;
-            } else {
-                choices.add(userChoice);
-            }
-
-            if (counter % 2 == 0) {
-                board.setField(userChoice, firstPlayer.getPlayerSign());
-                if (board.checkIfWinner(firstPlayer.getPlayerSign(), firstPlayer.getNickName())) {
-                    break;
+                if (choices.contains(userChoice)) {
+                    System.out.println("This field ia unavailable, please choose another.");
+                    continue;
+                } else {
+                    choices.add(userChoice);
                 }
-                System.out.println("Now Player: " + "'" + secondPlayer.getNickName() + "'");
 
-            } else if (counter % 2 != 0) {
-                board.setField(userChoice, secondPlayer.getPlayerSign());
-                if (board.checkIfWinner(secondPlayer.getPlayerSign(), secondPlayer.getNickName())) {
-                    break;
+                if (currentPlayer == firstPlayer) {
+                    board.setField(userChoice, firstPlayer.getPlayerSign());
+                    if (board.checkIfWinner(firstPlayer.getPlayerSign(), firstPlayer.getNickName())) {
+                        break;
+                    }
+                    System.out.println("Now Player: " + "'" + secondPlayer.getNickName() + "'");
+
+                } else if (currentPlayer == secondPlayer) {
+                    board.setField(userChoice, secondPlayer.getPlayerSign());
+                    if (board.checkIfWinner(secondPlayer.getPlayerSign(), secondPlayer.getNickName())) {
+                        break;
+                    }
+                    System.out.println("Now Player: " + "'" + firstPlayer.getNickName() + "'");
                 }
-                System.out.println("Now Player: " + "'" + firstPlayer.getNickName() + "'");
-            }
-            board.printBoard();
-            counter++; // changing player
+                board.printBoard();
+                counter++; // changing player
 
-            System.out.println();
-            if (choices.size() == 9) {
-                System.out.println("It is a draw!");
+                System.out.println();
+                if (choices.size() == 9) {
+                    System.out.println("It is a draw!");
+                }
+                // Printing draw, when are 9 moves
             }
-            // Printing draw, when are 9 moves
         }
     }
 }
