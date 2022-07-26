0de5805a4536ea9a7ccf1e61e93a1ac78961c3e1
KamilChabros <kamilchabros@wp.pl>
2022-07-26 19:36:06 +0200

    Some changes in HumanPlayer class

--- src/main/java/HumanPlayer.java
+++ src/main/java/HumanPlayer.java
@@ -8,19 +8,4 @@ public class HumanPlayer extends Player {
         int move = sc.nextInt();
         return move;
     }
-//    Scanner sc = new Scanner(System.in);
-//
-//            System.out.println("Choose sign for First Player...(X, O, etc.)");
-//
-//        System.out.println("Provide nick for First Player: ");
-//
-//    // First Player
-//
-//        System.out.println("Choose sign for Second Player...(X, O, etc.)");
-//        System.out.println("Provide nick for Second Player: ");
-//    // Second Player
-//    // to-do Checking if first player has the same sign as second player
-//
-//        System.out.println("First Player is: " + firstPLayerNickName + " and has " + firstSign + " sign.");
-//        System.out.println("Second Player is: " + secondPLayerNickName + " and has " + secondSign + " sign.");
 }
